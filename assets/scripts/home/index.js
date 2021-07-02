9e928d646c96a3342c1d995ef343a20c35e8fb17
FaiezWaseem <78906502+FaiezWaseem@users.noreply.github.com>
2021-07-02 14:06:21 +0500

    updated pages

--- assets/scripts/home/index.js
+++ assets/scripts/home/index.js
@@ -10,7 +10,7 @@ menu.addEventListener('click', function () {
 
 DomEvent('#upload', "click", function(){
   if(islogin){
-  window.location.href = '../assets/Video_Upload.html'}
+  window.location.href = '/Video-Point/assets/Video_Upload.html'}
   else{
     a("You need to Login To Upload a video");
   }
@@ -18,9 +18,9 @@ DomEvent('#upload', "click", function(){
 DomEvent('#account','click',function()
 {
   if(islogin){
-    window.location.href = '../UserPanel/userpanel.html';
+    window.location.href = '/Video-Point/UserPanel/userpanel.html';
   }else{
-    window.location.href = '../Account/signup.html';
+    window.location.href = '/Video-Point/Account/signup.html';
   }
 })
 
@@ -140,11 +140,11 @@ function videoClicked(vid){
   var id = vid.getAttribute("data-id");
   //  c(id);
   // localStorage.setItem('key',id);
-  window.location.href = '../video view/video.html?page='+id;
+  window.location.href = '/Video-Point/video view/video.html?page='+id;
 }
 var element = get('.videos')
 var _x = 0
-setInterval(function(){
+var s = setInterval(function(){
   if (element.scrollHeight - element.scrollTop === element.clientHeight)
       {
         c("reached end");
@@ -172,8 +172,9 @@ setInterval(function(){
           
           });
           _x++;
+		clearInterval(s);
         }
 
       }
 
-},50000)
+},10000)
