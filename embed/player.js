dc26c523adb47bb61e5acff23e4955b79094f8c3
FaiezWaseem <78906502+FaiezWaseem@users.noreply.github.com>
2021-09-15 19:40:21 +0500

    fixingsome issue

--- embed/player.js
+++ embed/player.js
@@ -158,10 +158,3 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
